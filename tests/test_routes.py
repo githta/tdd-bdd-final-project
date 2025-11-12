a22f5faf5780370f7efde29dbc1d538050a566d3
githta <85226645+githta@users.noreply.github.com>
2025-11-12 15:10:36 +0700

    get a product test_routes.py

--- tests/test_routes.py
+++ tests/test_routes.py
@@ -178,3 +178,16 @@ class TestProductRoutes(TestCase):
         data = response.get_json()
         # logging.debug("data = %s", data)
         return len(data)
+
+    def test_get_product(self):
+        """It should get 1 product"""
+        test_product = self._create_products(1)[0]
+        response = self.client.get(f"{BASE_URL}/{test_product.id}")
+        self.assertEqual(response.status_code, status.HTTP_200_OK)
+        data = response.get_json()
+        self.assertEqual(data["name"], test_product.name)
+
+    def test_get_product_not_found(self):
+        """It should not get 1 product width id not found"""
+        response = self.client.get(f"{BASE_URL}/0")
+        self.assertEqual(response.status_code, status.HTTP_404_NOT_FOUND)
